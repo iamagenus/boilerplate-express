047e5f7943e8d4ac04a377e7aafa45bfcfcad314
iamagenus <132496206+iamagenus@users.noreply.github.com>
2024-02-08 18:40:41 +0000

    repair myapp.js

--- myApp.js
+++ myApp.js
@@ -1,12 +1,14 @@
-let express = require("express");
-let app = express();
-console.log("Hello World");
+var express = require("express");
+var app = express();
+
+app.use("/public", express.static(__dirname + "/public"));
+
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/views/index.html");
 });
+
 app.get("/json", function (req, res) {
   const mySecret = process.env["MESSAGE_STYLE"];
-
   if (mySecret === "uppercase") {
     res.json({ message: "Hello json".toUpperCase() });
   } else {
