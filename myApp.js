9abe69848a60e3c743955920bc820a0864f9706c
iamagenus <132496206+iamagenus@users.noreply.github.com>
2024-02-08 19:11:17 +0000

    save

--- myApp.js
+++ myApp.js
@@ -4,7 +4,6 @@ app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
-const mySecret = process.env["MESSAGE_STYLE"];
 app.get("/json", (req, res) => {
   res.json(
     process.env.MESSAGE_STYLE == "uppercase"
