81ff3d636918e9502bb2d3b698dc6d0716964c16
iamagenus <132496206+iamagenus@users.noreply.github.com>
2024-02-08 17:59:11 +0000

    ud

--- myApp.js
+++ myApp.js
@@ -2,6 +2,6 @@ let express = require("express");
 let app = express();
 console.log("Hello World");
 app.get("/", function (req, res) {
-  res.send("Hello Express");
+  res.send((absolutePath = __dirname + "/relativePath/file.ext"));
 });
 module.exports = app;
