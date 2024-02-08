df181862d12bea8c2d19904368a28edc42ccb357
iamagenus <132496206+iamagenus@users.noreply.github.com>
2024-02-08 18:53:31 +0000

    d

--- myApp.js
+++ myApp.js
@@ -1,15 +1,14 @@
-var express = require("express");
-var app = express();
-
+let express = require("express");
+let app = express();
 app.use("/public", express.static(__dirname + "/public"));
-
-app.get("/", function (req, res) {
+app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
 
 app.get("/json", (req, res) => {
-    if(process.env['MESSAGE_STYLE'] == 'uppercase'){
-    res.json({'message': 'HELLO JSON'})
-    }else{
-    res.json({'message': 'Hello json'})
-    }
+  if (process.env["MESSAGE_STYLE"] == "uppercase") {
+    res.json({ message: "HELLO JSON" });
+  } else {
+    res.json({ message: "Hello json" });
+  }
+});
