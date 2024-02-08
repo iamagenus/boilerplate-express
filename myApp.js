1bfe5db0bab939b728833f20e10e000b6c0a711f
iamagenus <132496206+iamagenus@users.noreply.github.com>
2024-02-08 18:06:57 +0000

    f

--- myApp.js
+++ myApp.js
@@ -2,6 +2,6 @@ let express = require("express");
 let app = express();
 console.log("Hello World");
 app.get("/", function (req, res) {
-  res.sendFile((absolutePath = "/" + "views/index.html"));
+  res.sendFile((absolutePath = "views/index.html"));
 });
 module.exports = app;
