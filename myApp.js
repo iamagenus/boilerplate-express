b04bf83329a6b46b6c9a72b4555c04651a2cba88
iamagenus <132496206+iamagenus@users.noreply.github.com>
2024-02-08 19:52:04 +0000

    fx

--- myApp.js
+++ myApp.js
@@ -1,6 +1,6 @@
 let express = require("express");
 let app = express();
-const mySecret = process.env["MESSAGE_STYLE"];
+
 app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
