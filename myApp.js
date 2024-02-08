8478841e16c3edd54279ea34f9d8f726fe2999f7
iamagenus <132496206+iamagenus@users.noreply.github.com>
2024-02-08 19:53:31 +0000

    s

--- myApp.js
+++ myApp.js
@@ -1,13 +1,13 @@
 let express = require("express");
 let app = express();
-
+const MESSAGE_STYLE = "uppercase";
 app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
 app.get("/json", (req, res) => {
   let response = { message: "Hello json" };
-  if (process.env.MESSAGE_STYLE === uppercase) {
+  if (process.env.MESSAGE_STYLE === "uppercase") {
     response.message = response.message.toUpperCase();
   }
   res.json(response);
