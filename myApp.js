e1cd3c99bb34b42398295fd6f903ef1b6ec90a0d
iamagenus <132496206+iamagenus@users.noreply.github.com>
2024-02-08 18:04:22 +0000

    dsf

--- myApp.js
+++ myApp.js
@@ -2,6 +2,6 @@ let express = require("express");
 let app = express();
 console.log("Hello World");
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + "/views/index.html");
+  res.sendFile("/" + "/views/index.html");
 });
 module.exports = app;
