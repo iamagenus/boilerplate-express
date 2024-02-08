1c740d28f8ed4d70d4be782b609b06aa0fc7cc0f
iamagenus <132496206+iamagenus@users.noreply.github.com>
2024-02-08 19:54:04 +0000

    f

--- myApp.js
+++ myApp.js
@@ -1,6 +1,6 @@
 let express = require("express");
 let app = express();
-const MESSAGE_STYLE = "uppercase";
+
 app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
