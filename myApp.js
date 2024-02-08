a9c7a351f7c3135866fc643ce48ef7c5307b1c95
iamagenus <132496206+iamagenus@users.noreply.github.com>
2024-02-08 19:24:31 +0000

    save

--- myApp.js
+++ myApp.js
@@ -1,5 +1,6 @@
 let express = require("express");
 let app = express();
+
 app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
