dc82793b9ea6b644e912b8452460d18d81ba3a73
iamagenus <132496206+iamagenus@users.noreply.github.com>
2024-02-08 17:54:52 +0000

    ud

--- myApp.js
+++ myApp.js
@@ -1,8 +1,7 @@
 let express = require("express");
 let app = express();
 console.log("Hello World");
-app.get("/", (req, res) => {
+app.get("/", function (req, res) {
   res.send("Hello Express");
 });
-
 module.exports = app;
